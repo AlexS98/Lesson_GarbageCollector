b845dfa9307ef531a98ef0c60b289bddb6f42dc6
Alexander <sania.sviridenko@gmail.com>
2019-07-30 23:35:30 +0300

    Add project files.

---
+++ Lesson_GarbageCollector/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lesson_GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader streamReader = new StreamReader("example.txt"))
+            {
+                Console.WriteLine(
+                    streamReader
+                        .ReadToEnd()
+                        .Split(';')
+                        .ToList()
+                        .Select(x => long.TryParse(x, out long l) ? l : 0)
+                        .Sum()
+                    );
+            }
+
+            for (int i = 0; i < 20000; i++)
+            {
+                var test = new TestClass($"object{i}");
+            }
+        }
+    }
+}
---
+++ Lesson_GarbageCollector/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_GarbageCollector
+{
+    public class TestClass : IDisposable
+    {
+        public string Name { get; set; }
+
+        public TestClass(string name)
+        {
+            Name = name;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"\t{Name} disposed.");
+        }
+
+        ~TestClass()
+        {
+            Console.WriteLine($"\t{Name} finalized.");
+        }
+    }
+}
